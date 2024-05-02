21464d448937b0499e0a655227e777eedf41d0ce
oAllanWeb <allanweb019@gmail.com>
2024-05-02 16:35:53 -0300

    renomaer aulas

--- index.js
+++ index.js
@@ -2,12 +2,10 @@ const aulasWeb = './web/aulas';
 const aulasDesktop = './desktop/aulas';
 const fs = require('fs');
 
-const gera_aulas = function() {
-    
-    fs.readdir(aulasWeb, (err, filesWeb) => {
+const gera_aulas = function () {
+  fs.readdir(aulasWeb, (err, filesWeb) => {
     fs.readdir(aulasDesktop, (err, filesDesktop) => {
-
-        let template = `<!DOCTYPE html>
+      let template = `<!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
@@ -20,26 +18,36 @@ const gera_aulas = function() {
                 <h1>Aulas 5º Período</h1>
                 <h2>Aulas WEB </h2>
                 <ul id="aulas-web">
-                ${filesWeb.filter((file)=> file.includes('.html')).map((file)=> `<li><a href="/web/aulas/${file}">${file.replace('.html','')}</a></li></br>` )}
+                ${filesWeb
+                  .filter((file) => file.includes('.html'))
+                  .reduce(
+                    (prev, curr) =>
+                      (prev += `<li><a href="/web/aulas/${curr}">${curr.replace(
+                        '.html',
+                        ''
+                      )}</a></li></br>`),
+                    ''
+                  )}
                 </ul>
                 <h2>Aulas Desktop</h2>
                 <ul id="aulas-desktop">
-                    ${filesDesktop.filter((file)=> file.includes('.html')).map((file)=> `<li><a href="/desktop/aulas/${file}">${file.replace('.html','')}</a></li></br>` )}
+                    ${filesDesktop
+                      .filter((file) => file.includes('.html'))
+                      .reduce(
+                        (prev, curr) =>
+                          (prev += `<li><a href="/desktop/aulas/${curr}">${curr.replace(
+                            '.html',
+                            ''
+                          )}</a></li></br>`),
+                        ''
+                      )}
                 </ul>
             </section>
         </body>
-        </html>`
-        fs.writeFileSync('./index.html', template)
+        </html>`;
+      fs.writeFileSync('./index.html', template);
     });
-    });
+  });
+};
 
-    
-
-    
-}
-
-
-
-
-
-gera_aulas()
+gera_aulas();
