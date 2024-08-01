6e705d9350fa3871d5bef943352b7ea178aaf5f3
Allan da Silva <allan.silva@newfold.com>
2024-08-01 18:04:55 -0300

    feat: add aula teste unipar

--- index.js
+++ index.js
@@ -1,52 +1,37 @@
-const aulasWeb = './web/aulas';
-const aulasDesktop = './desktop/aulas';
+const aulasTest = './teste/aulas';
 const fs = require('fs');
 
 const gera_aulas = function () {
-  fs.readdir(aulasWeb, (err, filesWeb) => {
-    fs.readdir(aulasDesktop, (err, filesDesktop) => {
-      let template = `<!DOCTYPE html>
+  fs.readdir(aulasTest, (err, filesTeste) => {
+    let template = `<!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>ADS 5º Período </title>
+            <title>ADS 6º Período </title>
             <link href="./style.css" rel="stylesheet"/>
         </head>
         <body>
             <section>
-                <h1>Aulas 5º Período</h1>
-                <h2>Aulas WEB </h2>
+                <h1>Aulas 6º Período</h1>
+                <h2>Aulas Testes </h2>
                 <ul id="aulas-web">
-                ${filesWeb
+                ${filesTeste
                   .filter((file) => file.includes('.html'))
                   .reduce(
                     (prev, curr) =>
-                      (prev += `<li><a href="/web/aulas/${curr}">${curr.replace(
-                        '.html',
-                        ''
-                      )}</a></li></br>`),
+                      (prev += `<li><a href="/teste/aulas/${curr}">${curr
+                        .replace('.html', '')
+                        .split('-')
+                        .join(' ')}</a></li></br>`),
                     ''
                   )}
                 </ul>
-                <h2>Aulas Desktop</h2>
-                <ul id="aulas-desktop">
-                    ${filesDesktop
-                      .filter((file) => file.includes('.html'))
-                      .reduce(
-                        (prev, curr) =>
-                          (prev += `<li><a href="/desktop/aulas/${curr}">${curr.replace(
-                            '.html',
-                            ''
-                          )}</a></li></br>`),
-                        ''
-                      )}
-                </ul>
+                
             </section>
         </body>
         </html>`;
-      fs.writeFileSync('./index.html', template);
-    });
+    fs.writeFileSync('./index.html', template);
   });
 };
 
